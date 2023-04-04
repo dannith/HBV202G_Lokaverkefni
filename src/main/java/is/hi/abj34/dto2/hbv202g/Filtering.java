5f675de38bb1dac94366028721aea9e6034d11ed
abj34 <abj34@hi.is>
2023-04-04 12:23:39 +0000

    work started v2

---
+++ src/main/java/is/hi/abj34/dto2/hbv202g/Filtering.java
@@ -0,0 +1,5 @@
+package is.hi.abj34.dto2.hbv202g;
+
+public class Filtering {
+    
+}
--- src/main/java/is/hi/abj34/dto2/hbv202g/QuestionSystem.java
+++ src/main/java/is/hi/abj34/dto2/hbv202g/QuestionSystem.java
@@ -10,16 +10,16 @@ public class QuestionSystem {
         this.categories = new ArrayList<>();
     }
 
-    // Add Category
+    // Add Category - while loop um hvað þú vilt velja
 
-    // Add Question and answer?
+    // Add Question and options
 
-    // Choose category and how many questions
+    // Choose category and how many questions to answer - sér java file - Filtering.java
 
-    // Display question or choose what questions will be asked
+    // Display question or choose what questions will be asked - Sama of fyrir ofan - Filtering.java
 
-    // Answer question and whether right
+    // Answer question and whether right - sér java file - Answering.java
 
-    // Scores for questions
+    // Scores for questions - Sama og fyrir ofan - Answering.java
 
 }
